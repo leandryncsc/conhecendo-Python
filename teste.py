0265c2827ea4480d57bcae633e277bfc6214a1b0
leandryncsc <leandrinhodoema@gmail.com>
2023-10-03 14:42:29 -0300

    novos testes

--- teste.py
+++ teste.py
@@ -1,9 +1,20 @@
-from turtle import*
-speed(10)
-color("yellow")
-bgcolor("green")
-b=200
-while b > 0:
-    left(b)
-    forward(b*3)
-    b=b-1
+import os
+from random import randint
+
+user_pass = input("digite sua senha: ")
+
+key = ['1', '2','3','4','5','6','7','8','9','10',
+       'a','b','c','d','e','f','g','h','i','j',
+       'k','l','m','n','o','p','q','r','s','t',
+       'u','v','w','x','y','z','@','#','$','%','&']
+
+guess = ""
+
+while (guess != user_pass):
+    guess = ""
+    for letter in range(len(user_pass)):
+        guess_letter = key[randint(0,25)]
+        guess = str(guess_letter) + str(guess)
+        print(guess)
+
+print("sua senha é", guess)
